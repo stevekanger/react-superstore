4e1188298f892a3bb58af82c4344dfd5e17c69ae
SteveKanger <hi.stevekanger@gmail.com>
2021-10-29 23:14:50 -0500

    Cleanup checkkeys

--- createStore.js
+++ createStore.js
@@ -5,8 +5,7 @@ const isFn = (fn) => typeof fn === 'function'
 const isObject = (obj) => typeof obj === 'object' && obj != null
 
 const checkKeys = (keys, store, oldStore) => {
-  if (keys.length < 1) return true
-  if (!isObject(store) || !isObject(oldStore)) return true
+  if (keys.length < 1 || !isObject(store) || !isObject(oldStore)) return true
 
   for (let i = 0; i < keys.length; i++) {
     if (keys[i] in store && store[keys[i]] !== oldStore[keys[i]]) {
