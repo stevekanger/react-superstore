535fbaf46eecb74fdda03889d442d7fbfc214731
SteveKanger <hi.stevekanger@gmail.com>
2023-03-08 16:20:45 -0600

    Removed function check from isObj

--- src/utils/isObj.ts
+++ src/utils/isObj.ts
@@ -1,7 +1,4 @@
 const isObj = (obj: {}) =>
-  typeof obj === 'object' &&
-  typeof obj !== 'function' &&
-  !Array.isArray(obj) &&
-  obj !== null
+  typeof obj === 'object' && !Array.isArray(obj) && obj !== null
 
 export default isObj
